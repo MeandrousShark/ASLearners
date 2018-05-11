d7a1b708a9222d169d0ca0f73f459eca5e9958ed
Michael Patrick Hannon <hannonm2@LabImage2016-17>
2018-05-11 12:30:10 -0700

    initial commit

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+package hannonm2.aslearn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
